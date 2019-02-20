2d79161609cecfe42132e2a2a014be77fca1cd21
MEBonham <programmer@mckaybonham.com>
2019-02-20 15:04:25 -0700

    Climbing_stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,12 +3,26 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+    
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    if not cache:
+        cache = [0 for i in range(n + 1)]
+    
+    if cache[n]:
+        return cache[n]
+
+    total = climbing_stairs(n - 1, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 3, cache)
+    cache[n] = total
+    return total
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
